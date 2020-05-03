ee4764cce7dd1dcd1647f25182f8442c094c132f
insert-usernamehere <montyd18@gmail.com>
2020-05-03 17:40:27 -0400

    added word of the day

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const bot = new Discord.Client();
 const token = "bot token";
 
 bot.on("ready", () => {
-  console.log("bot is working");
+  console.log("bot is up and running if its not, start an issue plz give it 5 mins");
 });
 
 bot.on("message", async msg => {
@@ -25,6 +25,50 @@ bot.on("message", async msg => {
     console.log(jokeValue);
     msg.reply(`here a fact: ${jokeValue.data}`);
   }
+
+  if (msg.content === "useless fact") {
+    let getFact = async () => {
+      let response = await axios.get(
+        "https://uselessfacts.jsph.pl/random.json?language=en"
+      );
+      let fact = response.data;
+      return fact;
+    };
+    let factValue = await getFact();
+    console.log(factValue);
+    msg.reply(`here a fact: ${factValue.text}`);
+  }
+
+  if (msg.content === "skawo") {
+    msg.reply("skawo is an amazing youtuber that does text comentary heres his channel: https://www.youtube.com/channel/UColqqqGEOAuzeD8Zt5Y67FQ ");
+  }
+
+  if (msg.content === "developer role") {
+    msg.reply("can I get developer role?");
+  }
+
+  if (msg.content === "location") {
+    msg.reply("you're on the moon duh");
+  }
+
+  if (msg.content === "( ͡° ͜ʖ ͡°)") {
+    msg.reply("ew how dare you ( ͡° ͜ʖ ͡°)");
+  }
+
+  if (msg.content === "word of the day") {
+    let getWord = async () => {
+      let response = await axios.get(
+        "http://urban-word-of-the-day.herokuapp.com/today"
+      );
+      let word = response.data;
+      return word;
+    };
+    let wordValue = await getWord();
+    console.log(wordValue);
+    msg.reply(`here the word of the day ${wordValue.word} and heres its definition ${wordValue.meaning}`);
+  }
+
+
 });
 
 bot.login(token);
